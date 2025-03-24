b94827313048240a9855580a86dd0d0052c7b82e
Sahil Malviya <jkjishankhan786@gmail.com>
2025-03-24 11:33:35 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: "/jishanarts.github.io", // Change this to your repo name
-  trailingSlash: true,
+const nextConfig: NextConfig = {
+  /* config options here */
 };
-
-module.exports = nextConfig;
-
 
 export default nextConfig;
